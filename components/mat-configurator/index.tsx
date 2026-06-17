4a194b4b9f900807625859a26d7e8b650936b707
thepieter22-web <thepieter22@gmail.com>
2026-06-17 09:56:55 +0200

    Update cart redirect URL to checkout page

--- components/mat-configurator/index.tsx
+++ components/mat-configurator/index.tsx
@@ -124,7 +124,7 @@ export function MatConfigurator() {
 
   
 const handleAddToCart = () => {
-  window.top.location.href = "https://www.carpetz.be/?add-to-cart=5950&redirect_to=/checkout/";
+  window.top.location.href = "https://www.carpetz.be/checkout/?add-to-cart=5950";
 };
 ``
 
